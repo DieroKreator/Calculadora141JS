051b0075d8c77b6e08b105b810675ec8c513b910
DieroKreator <qtechwired@gmail.com>
2024-09-15 19:54:01 -0300

    Adicionar outras funcoes com massa json

--- __tests__/calculadoraMassa.spec.js
+++ __tests__/calculadoraMassa.spec.js
@@ -16,3 +16,39 @@ test.each(massa.array.map(bloco => [
         // Valida
         expect(resultadoObtido).toBe(resultadoEsperado)
     })
+
+// Subtrair
+test.each(massa.array.map(bloco => [
+    bloco.num1,
+    bloco.num2,
+    bloco.esperadoSubtracao]))
+    ('subtrair %f - %f com massa', (num1, num2, resultadoEsperado) => {
+        // Executa
+        const resultadoObtido = calculadora.subtrairDoisNumeros(num1, num2)
+        // Valida
+        expect(resultadoObtido).toBe(resultadoEsperado)
+    })
+
+// Multiplicar
+test.each(massa.array.map(bloco => [
+    bloco.num1,
+    bloco.num2,
+    bloco.esperadoMultiplicacao]))
+    ('multiplicar %f * %f com massa', (num1, num2, resultadoEsperado) => {
+        // Executa
+        const resultadoObtido = calculadora.multiplicarDoisNumeros(num1, num2)
+        // Valida
+        expect(resultadoObtido).toBe(resultadoEsperado)
+    })
+
+// Dividir
+test.each(massa.array.map(bloco => [
+    bloco.num1,
+    bloco.num2,
+    bloco.esperadoDivisao]))
+    ('dividir %f / %f com massa', (num1, num2, resultadoEsperado) => {
+        // Executa
+        const resultadoObtido = calculadora.dividirDoisNumeros(num1, num2)
+        // Valida
+        expect(resultadoObtido).toBe(resultadoEsperado)
+    })
